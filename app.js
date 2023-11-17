65f6812fc8867a8922ca8683ee3e7ccd6c3aa6b9
mibek-shrestha <mibekstha@gmail.com>
2023-11-17 18:20:56 +0545

    allblog and addblog frontend

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const app = express()
+const app = express();
+//telling node js to require and use .env
 
-app.listen(3000, () => {
-    console.log("node js project has started at port 3000")
+const test = require('dotenv')
+test.config()
+
+console.log(process.env.name);
+//sidai yastoo hunxa require('dotenv').config --j garda ni hunxa
+
+
+//say nodejs that we are using ejs, set everything 
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("allBlogs")
+})
+
+app.get("/addBlog", (req, res) => {
+    res.render("addBlog")
+})
+const PORT = process.env.port
+app.listen(PORT, () => {
+    console.log("node js project has started at port " + PORT)
 })
