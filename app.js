32dee07549083a8ca8d786a625905b995b2538cc
mibek-shrestha <mibekstha@gmail.com>
2023-11-18 17:11:28 +0545

    connected database and inserted the data

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 //telling node js to require and use .env
+require("./model/index.js")
 
-const test = require('dotenv')
+//telling nodejs to accept the incoming data (parsing data)
+// app.use(express.json()) react vue bata
+app.use(express.urlencoded({ extended: true }))
+
+
+const test = require('dotenv');
+const { blogs } = require("./model/index.js");
 test.config()
 
 console.log(process.env.name);
@@ -19,6 +26,19 @@ app.get("/", (req, res) => {
 app.get("/addBlog", (req, res) => {
     res.render("addBlog")
 })
+
+//api for handling form data 
+app.post("/addBlog", async (req, res) => {
+    // console.log(req.body);
+    await blogs.create({
+        title: req.body.title,
+        subTitle: req.body.subTitle,
+        description: req.body.description
+    })
+    res.send("Blog Created Sucessfully")
+})
+
+
 const PORT = process.env.port
 app.listen(PORT, () => {
     console.log("node js project has started at port " + PORT)
